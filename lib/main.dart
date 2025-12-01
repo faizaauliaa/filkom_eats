934145889302fbe9421b483a0a0de33f7fbf7995
Ninda <anindhitafaiza@gmail.com>
2025-12-02 00:16:20 +0700

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:filkom_eats/screens/home_page.dart';
+import 'package:filkom_eats/screens/menu_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/cart_provider.dart';
+import 'providers/order_provider.dart';
+import 'providers/favorite_provider.dart';
+import 'screens/splash_screens.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => OrderProvider()),
+        ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: const SplashScreen(),
+    );
+  }
+}
