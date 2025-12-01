e52a00a25cafba0b8126c2d9bb27a56c602e14ca
Ninda <anindhitafaiza@gmail.com>
2025-12-02 00:29:14 +0700

    Add SplashScreen with navigation to Onboarding1
    This file implements a SplashScreen that displays a logo and text before navigating to the onboarding screen after a delay.

---
+++ lib/screens/splash_screens.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'onboarding/onboarding1.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const Onboarding1()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xffff4d78),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset("assets/noodle.png", height: 135),
+            const SizedBox(height: 20),
+            const Text(
+              "FILKOM EATS",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
