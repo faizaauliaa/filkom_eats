372d37bddc5ff7bf8ddbadbc6c80b1306a4ea864
Ninda <anindhitafaiza@gmail.com>
2025-12-02 00:34:00 +0700

    Add Onboarding2 screen with delivery tracking info

---
+++ lib/screens/onboarding/onboarding2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'onboarding_template.dart';
+import 'onboarding3.dart';
+
+class Onboarding2 extends StatelessWidget {
+  const Onboarding2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OnboardingTemplate(
+      title: "Real-Time Delivery\nAvailability",
+      subtitle: "Track delivery availability from each canteen.",
+      nextPage: Onboarding3(),
+    );
+  }
+}
