8bff6e5fd6ccbf60beb849cafd870a711d6509a8
Ninda <anindhitafaiza@gmail.com>
2025-12-02 00:21:56 +0700

    Add Product model with properties and constructor

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id;
+  final String name;
+  final String image;
+  final int price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.price,
+  });
+}
