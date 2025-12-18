5cb37f36ee359b0b134d93cb63e2baa3eb3a30fc
Ninda <anindhitafaiza@gmail.com>
2025-12-18 16:14:41 +0700

    Implement navigation on sign in button press
    Updated the SsoLoginScreen to navigate to MainNavigation on sign in.

--- lib/screens/auth/sso_login_screen.dart
+++ lib/screens/auth/sso_login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:filkom_eats/screens/main_navigation.dart';
 
 class SsoLoginScreen extends StatelessWidget {
   const SsoLoginScreen({super.key});
@@ -17,13 +18,24 @@ class SsoLoginScreen extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 30),
-            TextField(decoration: InputDecoration(labelText: "Username")),
+            const TextField(decoration: InputDecoration(labelText: "Username")),
             const SizedBox(height: 14),
-            TextField(decoration: InputDecoration(labelText: "Password")),
+            const TextField(decoration: InputDecoration(labelText: "Password")),
             const SizedBox(height: 30),
-            ElevatedButton(onPressed: () {}, child: const Text("Sign In")),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const MainNavigation()),
+                );
+              },
+              child: const Text("Sign In"),
+            ),
+
             const SizedBox(height: 45),
             Text("Continue", style: TextStyle(color: Colors.grey[600])),
+
           ],
         ),
       ),
