fdac25249542d554b3989103bcd039f9e3ed4001
Ninda <anindhitafaiza@gmail.com>
2025-12-18 15:56:32 +0700

    Create order_item.dart

---
+++ lib/models/order_item.dart
@@ -0,0 +1,13 @@
+class OrderItem {
+  final String name;
+  final String note;
+  final String price;
+  final int qty;
+
+  OrderItem({
+    required this.name,
+    required this.note,
+    required this.price,
+    required this.qty,
+  });
+}
